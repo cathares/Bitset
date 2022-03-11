e8b801ea610172f762d3912b7f8c4b6bbdc0ca01
Alex <kennycrank@yandex.ru>
2022-03-11 20:34:55 +0300

    Bitset finished.

--- src/main/java/Bitset.java
+++ src/main/java/Bitset.java
@@ -1,8 +1,5 @@
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
-import static java.lang.Integer.valueOf;
 
 /*
 Вариант 18 -- bitset
@@ -11,91 +8,143 @@ import static java.lang.Integer.valueOf;
 Конкретный элемент набора идентифицируется неотрицательным целым от нуля до количества элементов - 1 (альтернатива -- уникальным именем).
 Операции: пересечение, объединение, дополнение; добавление/удаление заданного элемента (массива элементов), проверка принадлежности элемента множеству.
 Бонус: итератор по множеству.
- */
-public final class Bitset implements Iterable<Integer>{
+*/
 
-    private final ArrayList<Integer> elements;
+
+public class Bitset implements Iterable<Integer> {
+
+    private final ArrayList<Boolean> elements;
+
+    public static class Universum {
+
+        private final Bitset set;
+
+        public Universum(Bitset set) {
+            for (int i = 0; i < set.elements.size(); i++) {
+                set.elements.set(i, !set.elements.get(i));
+            }
+            this.set = set;
+        }
+
+    }
 
     public Bitset(int size) {
         elements = new ArrayList<>();
-        for (int i = 0; i < size; i++) {
-            elements.add(i);
+        for (int i=0; i < size; i++) {
+            elements.add(i, true);
         }
-    }
-
-    public Bitset(ArrayList<Integer> elements) {
-        this.elements = elements;
     }
 
     public List<Integer> getSet(){
-        return elements;
+        ArrayList<Integer> res = new ArrayList<>();
+        for (int i = 0; i < elements.size(); i++) {
+            if (elements.get(i)) {
+                res.add(i);
+            }
+        }
+        return res;
     }
 
-    public void  add(int elem) {
-        if (!elements.contains(elem)) {
-            elements.add(elem);
+
+    public Bitset(List<Integer> elems) {
+        elements = new ArrayList<>();
+        for (int i = 0; i <= Collections.max(elems); i++) {
+            if (elems.contains(i)) {
+                elements.add(i, true);
+            }
+            else {
+                elements.add(i, false);
+            }
         }
     }
-
-    public void add(List<Integer> ListOfElem) {
-        for (int elem: ListOfElem) {
-            if (!elements.contains(elem)){
-                elements.add(elem);
+    public void add(int elem) {
+        if (elem >= this.elements.size()) {
+            for (int i = this.elements.size(); i < elem; i++) {
+                elements.add(i, false);
             }
+            elements.add(elem, true);
+        }
+        else elements.set(elem, true);
+    }
+
+    public void add(List<Integer> elems) {
+        for (int elem: elems) {
+            if (elem >= this.elements.size()) {
+                for (int i = this.elements.size(); i < elem; i++) {
+                    elements.add(i, false);
+                }
+                elements.add(elem, true);
+            }
+            else elements.set(elem, true);
         }
     }
 
     public void remove(int elem) {
-        if (elements.contains(elem)) {
-            elements.remove(valueOf(elem));
-        }
+        if (elem < this.elements.size())
+        elements.set(elem, false);
     }
 
-    public void remove(List<Integer> ListOfElem) {
-        for (int elem: ListOfElem) {
-            if (elements.contains(elem)) {
-                elements.remove(valueOf(elem));
-            }
+    public void remove(List<Integer> elems) {
+        for (int elem: elems) {
+            if (elem < this.elements.size())
+                elements.set(elem, false);
         }
     }
 
     public boolean contains(int elem) {
-        return elements.contains(elem);
-    }
-
-    public Bitset intersect(Bitset other) {
-        ArrayList<Integer> res = new ArrayList<>();
-        for (int elem: this.elements) {
-            if (other.contains(elem)) {
-                res.add(elem);
-            }
-        }
-        return new Bitset(res);
+        return elements.size() > elem && elements.get(elem);
     }
 
     public Bitset union(Bitset other) {
-        other.add(this.elements);
-        return other;
+        for (int i = 0; i < other.elements.size(); i++) {
+            if (this.elements.size() > i) {
+                if (other.elements.get(i)) {
+                    this.elements.set(i, true);
+                }
+            }
+            else this.elements.add(i, other.elements.get(i));
+        }
+        return this;
     }
 
-    public Bitset complement(Bitset other) {
-        for (int elem: this.elements) {
-            if (other.contains(elem)) other.remove(elem);
+    public Bitset intersect(Bitset other) {
+        for (int i = 0; i < this.elements.size(); i++) {
+            if (other.elements.size() <= i || !other.elements.get(i)) {
+                this.elements.set(i, false);
+            }
         }
+        return this;
+    }
+
+    public Universum complement(){
+        return new Universum(this);
+    }
+
+    public Bitset intersect(Universum other) {
+        for (int i = 0; i < this.elements.size(); i++) {
+            if (!(this.contains(i) && (other.set.contains(i) || other.set.elements.size() <= i))) {
+                this.elements.set(i, false);
+            }
+        }
+        return this;
+    }
+
+    public Universum union(Universum other) {
+        other.set.union(this);
         return other;
     }
 
     @Override
-    public Iterator <Integer> iterator() {
-        return new MyIterator<>(elements);
+    public Iterator<Integer> iterator() {
+        return new MyIterator<Integer>(this.getSet());
     }
 
     public static class MyIterator<Integer> implements Iterator<Integer> {
 
         int curr = 0;
-        ArrayList<Integer> list;
+        List<Integer> list;
 
-        public MyIterator(ArrayList<Integer> list) {
+        public MyIterator(List<Integer> list) {
             this.list = list;
         }
 
--- src/test/java/BitsetTest.java
+++ src/test/java/BitsetTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -47,6 +48,18 @@ public class BitsetTest {
         ArrayList<Integer> expectedRes = new ArrayList<>(Arrays.asList(0,1,2,3,4));
         Assertions.assertEquals(expectedRes, set1.intersect(set2).getSet());
 
+        set1 = new Bitset(Arrays.asList(5,6,7));
+        set2 = new Bitset(Arrays.asList(3,4,5,6));
+        Bitset set3 = new Bitset(Arrays.asList(0,1));
+        expectedRes = new ArrayList<>(Arrays.asList(0,1));
+        Assertions.assertEquals(expectedRes, set3.intersect(set1.union(set2.complement())).getSet());
+
+        set1 = new Bitset(Arrays.asList(0,1,2,5));
+        set2 = new Bitset(Arrays.asList(3,5,7));
+        set3 = new Bitset(Arrays.asList(2,3));
+        expectedRes = new ArrayList<>(List.of(2));
+        Assertions.assertEquals(expectedRes, set1.intersect(set3.intersect(set2.complement())).getSet());
+
     }
 
     @Test
@@ -55,6 +68,25 @@ public class BitsetTest {
         Bitset set2 = new Bitset(10);
         ArrayList<Integer> expectedRes = new ArrayList<>(Arrays.asList(0,1,2,3,4,5,6,7,8,9));
         Assertions.assertEquals(expectedRes, set1.union(set2).getSet());
+
+        set1 = new Bitset(Arrays.asList(0,1,2,3,5));
+        set2 = new Bitset(Arrays.asList(7,4,2,3));
+        Bitset set3 = new Bitset(Arrays.asList(10,3,6,12));
+        expectedRes = new ArrayList<>(Arrays.asList(0,1,2,3,5,6,10,12));
+        Assertions.assertEquals(expectedRes, set1.union(set3.intersect(set2.complement())).getSet());
+
+        set1 = new Bitset(Arrays.asList(0,2,4,6,5));
+        set2 = new Bitset(Arrays.asList(0,1,2,3,4,5,6));
+        set3 = new Bitset(Arrays.asList(7,8,9,10,20));
+        Bitset set4 = new Bitset(Arrays.asList(11,21));
+        expectedRes = new ArrayList<>(Arrays.asList(11,21));
+        Assertions.assertEquals(expectedRes, set4.intersect(set1.union(set2.union(set3)).complement()).getSet());
+
+        set1 = new Bitset(Arrays.asList(0,2,4,6,5));
+        expectedRes = new ArrayList<>(Arrays.asList(2,4,5,6));
+        set1.remove(0);
+        Assertions.assertEquals(expectedRes, set1.union(set1).getSet());
+
     }
 
     @Test
@@ -62,13 +94,14 @@ public class BitsetTest {
         Bitset set1 = new Bitset(5);
         Bitset set2 = new Bitset(10);
         ArrayList<Integer> expectedRes = new ArrayList<>(Arrays.asList(5,6,7,8,9));
-        Assertions.assertEquals(expectedRes, set1.complement(set2).getSet());
+        Assertions.assertEquals(expectedRes, set2.intersect(set1.complement()).getSet());
+
     }
 
     @Test
     public void iterator(){
         Bitset set1 = new Bitset(5);
-        Bitset set2 = new Bitset(0);
+        Bitset set2 = new Bitset(1);
         for (int elem: set1) {
             set2.add(elem);
         }
